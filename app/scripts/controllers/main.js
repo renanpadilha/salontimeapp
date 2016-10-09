e88e7ef5cea638f3127aadd84ca3387dbf686d4f
Renan Padilha <renanpadilha94@hotmail.com>
2016-10-08 21:47:43 -0300

    front ajustado e carregado novos servicos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the salontimeApp
  */
 angular.module('salontimeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $routeParams, Main) {
+    $scope.agendamento = {};
+    Main.getServicos().then(function(response){
+        $scope.servicos = response.data;
+    });
+
+    Main.getEstabelecimentoByIdService({id: $scope.servicoSelecionado}).then(function(response){
+        $scope.estabelecimentos = response.data;
+        console.log('LOG', $scope.estabelecimentos);
+    });
+
+    console.log('LOG', $scope.estabelecimentos);
+    console.log($scope.servicoSelecionado);
+
   });
---
+++ app/scripts/services/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name salontimeApp.estabelecimentos
+ * @description
+ * # estabelecimentos
+ * Service in the salontimeApp.
+ */
+angular.module('salontimeApp')
+  .service('Main', function ($http, $routeParams) {
+    const API_URL = 'http://localhost:9002/api/v1';
+    this.getServicos = function(){
+      return $http.get(API_URL + '/servicos');
+    };
+    this.getEstabelecimentoByIdService = function(){
+      return $http.get(API_URL + '/servicos/:id/estabelecimentos');
+    };
+
+  });
