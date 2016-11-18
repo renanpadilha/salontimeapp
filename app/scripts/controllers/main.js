f4536066bdea4308758c544e30dc7e7ad5b1b0da
Renan Padilha <renan.padilha@zeta.com.br>
2016-11-18 18:22:05 -0200

    Ajustado hora

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -34,23 +34,19 @@ angular.module('salontimeApp')
 
     };
 
-    //@TODO melhorar essa data/hora, por hora está funcionando
-    var date = moment($scope.date).format('YYYY-MM-DD');
-    var time = moment($scope.time).format('HH:mm:ss');
-    $scope.data =  date + ' ' + time;
+    $scope.valorData = function(){
+    };
 
-    console.log('dataHORA', $scope.datahora);
 
     $scope.agendar = function(){
+      var dataLocal = moment($scope.datahora).local().format();
       //@TODO O ID DO USUÁRIO DEVE SER O ID DE QUEM ESTÁ LOGADO NA APLICAÇÃO
       var dadosAgendamento = {
         id_estabelecimento: $scope.estabelecimentos[0].id,
         id_profissional: $scope.profissionais[0].id,
         id_servico: $scope.servicos[0].id,
-        data: $scope.datahora
+        data: dataLocal
       };
-
-      console.log(dadosAgendamento);
 
       if(!dadosAgendamento) {
         console.log('Não foram encontrados dados para solicitação');
@@ -61,43 +57,4 @@ angular.module('salontimeApp')
         console.log($scope.mensagem);
       });
     };
-
-    /* DATEPICKER + TIMEPICKER */
-    $scope.dateOptions = {
-      dateDisabled: disabled,
-      showWeeks: false,
-      formatYear: 'yyyy',
-      maxDate: new Date(2020, 5, 22),
-      minDate: new Date(),
-      startingDay: 1
-    };
-
-    // Disable weekend selection
-    function disabled(data) {
-      var date = data.date,
-        mode = data.mode;
-      return mode === 'day' && (date.getDay() === 0);
-    }
-
-    $scope.open = function() {
-      $scope.popup.opened = true;
-    };
-
-    $scope.setDate = function(year, month, day) {
-      $scope.date = new Date(year, month, day);
-    };
-
-    $scope.format = 'dd/MM/yyyy';
-
-    $scope.popup = {
-      opened: false
-    };
-
-    $scope.time = new Date();
-    $scope.time.setMinutes(0);
-    $scope.time.setSeconds(0);
-    $scope.hstep = 1;
-    $scope.mstep = 30;
-    $scope.ismeridian = false;
-  /* FINAL DATEPICKER + TIMEPICKER */
 });
