44c1db0c032179859de786a6853d08fb55654d35
Renan Padilha <renanpadilha94@hotmail.com>
2016-11-17 23:41:25 -0200

    Atualizando

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://salontime.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
