bf0499bbf82a25a4d34477fa79fb4193b8adcb63
Renan Padilha <renan.padilha@zeta.com.br>
2016-11-18 15:28:30 -0200

    Alteração de permissão para acesso a API

--- app.js
+++ app.js
@@ -23,9 +23,10 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://salontime.herokuapp.com/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Location, Authorization');
+    res.setHeader('Access-Control-Expose-Headers', 'Location');
     next();
 });
 
