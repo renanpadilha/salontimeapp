ca592e6cdf682d4f68f788dc7206918c8509afac
Renan Padilha <renan.padilha@zeta.com.br>
2017-03-01 14:26:12 -0300

    Adicionado index no serviço e tabela de agendamentos de cliente adicionada

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post('/api/v1/clientes/:id/agendamentos', function(req, res){
 /* LISTA TODOS OS AGENDAMENTOS DE UM CLIENTE*/
 app.get('/api/v1/clientes/:id/agendamentos', function(req, res){
   var id_cliente = req.params.id;
-  knex.raw("SELECT s.nome AS servicoNome, a.data AS dataAgendamento, e.nome AS estabelecimentoNome, p.nome AS profissionalNome FROM agendamento a JOIN estabelecimento e ON a.id_estabelecimento = e.id JOIN servico s ON a.id_servico = s.id JOIN profissional p ON a.id_profissional = p.id WHERE id_cliente = ?", id_cliente)
+  knex.raw("SELECT a.id, s.nome AS servicoNome, a.data AS dataAgendamento, e.nome AS estabelecimentoNome, p.nome AS profissionalNome FROM agendamento a JOIN estabelecimento e ON a.id_estabelecimento = e.id JOIN servico s ON a.id_servico = s.id JOIN profissional p ON a.id_profissional = p.id WHERE id_cliente = ?", id_cliente)
   .then(function (agendamentos) {
     res.json(agendamentos.rows);
   }).catch(function(err) {
