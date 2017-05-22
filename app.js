633c88a2efcabcd8e910e754adbbf8f4c1d3e917
Renan Padilha <renanpadilha94@hotmail.com>
2017-05-22 12:29:15 -0300

    Ajustes nos erviço

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ app.get('/api/v1/clientes/:id_cliente/estabelecimentos', function(req, res) {
 
 app.get('/api/v1/clientes/:id_cliente/favoritos', function(req, res) {
 	var id_cliente = req.params.id_cliente;
-	knex.raw('SELECT f.id, f.favorito, e.nome, e.id as estabelecimento, e.rate FROM favoritos f JOIN estabelecimentos e ON f.id_estabelecimento = e.id WHERE f.id_cliente = ?', id_cliente)
+	knex.raw('SELECT f.id, e.nome, e.id as estabelecimento, e.rate FROM favoritos f JOIN estabelecimentos e ON f.id_estabelecimento = e.id WHERE f.id_cliente = ?', id_cliente)
 	.then(function(favoritos) {
 		res.json(favoritos.rows);
 	}).catch(function(error) {
